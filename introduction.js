e8b060df37500080403634c29fde3f8d1c7ad446
FBW28 <fbw28@ubuntu.localdomain>
2020-05-12 12:15:54 +0200

    livecoding

---
+++ introduction.js
@@ -0,0 +1,13 @@
+// Comment one line JavaScript :) 
+
+/* 
+    More than one 
+    line Comment 
+    in JavaScript 
+*/
+
+/* Oneliner */
+
+console.log('Hello'); // Creates a new line
+console.log('world'); // even if its empty 
+console.log('!'); // console.log()
